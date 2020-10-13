a87843979e125818ab91cc5e8b423e3e1396bdad
Volodymyr Khaidarov <37961265+khaidvol@users.noreply.github.com>
2020-10-13 16:53:53 +0300

    small fixes.

--- src/main/java/com/epam/cdp/m2/hw2/aggregator/Java7ParallelAggregator.java
+++ src/main/java/com/epam/cdp/m2/hw2/aggregator/Java7ParallelAggregator.java
@@ -1,19 +1,17 @@
 package com.epam.cdp.m2.hw2.aggregator;
 
+import javafx.util.Pair;
+
 import java.util.List;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 
-import javafx.util.Pair;
-
 public class Java7ParallelAggregator implements Aggregator {
-
-    private static final int NUMBER_OF_THREADS = Runtime.getRuntime().availableProcessors();
 
     @Override
     public int sum(List<Integer> numbers) {
-        ForkJoinPool forkJoinPool = new ForkJoinPool(NUMBER_OF_THREADS);
-        int sum = forkJoinPool.invoke(new SumFork(numbers, 0, numbers.size()));
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        int sum = forkJoinPool.invoke(new SumRecursiveTask(numbers, 0, numbers.size()));
         return sum;
     }
 
@@ -28,37 +26,38 @@ public class Java7ParallelAggregator implements Aggregator {
     }
 
 
+    class SumRecursiveTask extends RecursiveTask<Integer> {
 
-    class SumFork extends RecursiveTask<Integer> {
-        private final int THRESHOLD = 250_000;
+        private final int THRESHOLD = 125_000;
         private List<Integer> list;
         private int begin;
         private int end;
 
-        public SumFork(List<Integer> list, int begin, int end) {
+        public SumRecursiveTask(List<Integer> list, int begin, int end) {
             this.list = list;
             this.begin = begin;
             this.end = end;
 
         }
 
-
         @Override
         protected Integer compute() {
-            final int size = end - begin;
-            if (size < THRESHOLD) {
+            if ((end - begin) < THRESHOLD) {
                 int sum = 0;
                 for(int i = begin; i < end; i++) {
                     sum+= list.get(i);
                 }
                 return sum;
             } else {
-                final int middle = ((end + begin) / 2);
-                SumFork sum1 = new SumFork(list, begin, middle);
-                sum1.fork();
-                SumFork sum2 = new SumFork(list, middle, end);
-                int sum =  sum2.compute() + sum1.join();
-                return sum;
+                int middle = ((end + begin) / 2);
+                SumRecursiveTask t1 = new SumRecursiveTask(list, begin, middle);
+                SumRecursiveTask t2 = new SumRecursiveTask(list, middle, end);
+                t1.fork();
+                t2.fork();
+                int res = 0;
+                res += t1.join();
+                res += t2.join();
+                return res;
             }
         }
     }
